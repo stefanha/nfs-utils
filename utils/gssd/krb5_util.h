68f4b69f3b8c627d37f6d40c209702fb4f266a2e
Kevin Coffman <kwc@citi.umich.edu>
2007-03-30 18:32:19 -0400

    Remove the now unused functions
    Remove functions that are no longer used when when obtaining
    machine credentials.
    
    Signed-off-by: Kevin Coffman <kwc@citi.umich.edu>
    Signed-off-by: Neil Brown <neilb@suse.de>

--- utils/gssd/krb5_util.h
+++ utils/gssd/krb5_util.h
@@ -19,7 +19,6 @@ struct gssd_k5_kt_princ {
 
 void gssd_setup_krb5_user_gss_ccache(uid_t uid, char *servername);
 int  gssd_get_krb5_machine_cred_list(char ***list);
-int  gssd_refresh_krb5_machine_creds(void);
 void gssd_free_krb5_machine_cred_list(char **list);
 void gssd_setup_krb5_machine_gss_ccache(char *servername);
 void gssd_destroy_krb5_machine_creds(void);
