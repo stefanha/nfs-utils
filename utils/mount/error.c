548bbdc49b1f5dfbe7ba75d20c5791652dfeb125
Kinglong Mee <kinglongmee@gmail.com>
2014-06-01 12:51:02 -0400

    mount.nfs: print more useful error message
    When mounting spec of a regular file, mount.nfs print error message as,
    
    mount.nfs: mount point /mnt is not a directory
    mount.nfs: mount point /mnt/testfile is not a directory
    
    This patch lets mount.nfs print more useful message,
    
    mount.nfs: mount spec 127.0.0.1:/root/testfile or point /mnt is not a
    directory
    mount.nfs: mount point /mnt/testfile is not a directory
    
    Signed-off-by: Kinglong Mee <kinglongmee@gmail.com>
    Signed-off-by: Steve Dickson <steved@redhat.com>

--- utils/mount/error.c
+++ utils/mount/error.c
@@ -215,8 +215,12 @@ void mount_error(const char *spec, const char *mount_point, int error)
 				progname);
 		break;
 	case ENOTDIR:
-		nfs_error(_("%s: mount point %s is not a directory"),
-				progname, mount_point);
+		if (spec)
+			nfs_error(_("%s: mount spec %s or point %s is not a "
+				  "directory"),	progname, spec, mount_point);
+		else
+			nfs_error(_("%s: mount point %s is not a directory"),
+				  progname, mount_point);
 		break;
 	case EBUSY:
 		nfs_error(_("%s: %s is busy or already mounted"),
