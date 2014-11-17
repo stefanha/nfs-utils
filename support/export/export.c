stefanha/nfs-utils

commit 076dd803330d93819c6e7862f515a8fa3997a83b

exportfs: changes handling of unresolvable entries
The patch to nfs/exportfs to allow nfsd to start when
there are some, but not all, unresolvable entries in
/etc/exports.

Signed-off-by: Henrique Martins <linux@martins.cc>
Signed-off-by: Steve Dickson <steved@redhat.com>

support/export/export.c
@@ -76,15 +76,22 @@ export_read(char *fname)
 	struct exportent	*eep;
 	nfs_export		*exp;
 
+	int volumes = 0;
+
 	setexportent(fname, "r");
 	while ((eep = getexportent(0,1)) != NULL) {
 		exp = export_lookup(eep->e_hostname, eep->e_path, 0);
-		if (!exp)
-			export_create(eep, 0);
+		if (!exp) {
+			exp = export_create(eep, 0);
+			if (exp)
+				volumes++;
+		}
 		else
 			warn_duplicated_exports(exp, eep);
 	}
 	endexportent();
+	if (volumes == 0)
+		xlog(L_ERROR, "No file systems exported!");
 }
 
 /**

support/export/client.c
@@ -277,7 +277,7 @@ client_lookup(char *hname, int canonical)
 	if (htype == MCL_FQDN && !canonical) {
 		ai = host_addrinfo(hname);
 		if (!ai) {
-			xlog(L_ERROR, "Failed to resolve %s", hname);
+			xlog(L_WARNING, "Failed to resolve %s", hname);
 			goto out;
 		}
 		hname = ai->ai_canonname;

support/export/hostname.c
@@ -177,11 +177,11 @@ host_addrinfo(const char *hostname)
 	case 0:
 		return ai;
 	case EAI_SYSTEM:
-		xlog(D_GENERAL, "%s: failed to resolve %s: (%d) %m",
+		xlog(D_PARSE, "%s: failed to resolve %s: (%d) %m",
 				__func__, hostname, errno);
 		break;
 	default:
-		xlog(D_GENERAL, "%s: failed to resolve %s: %s",
+		xlog(D_PARSE, "%s: failed to resolve %s: %s",
 				__func__, hostname, gai_strerror(error));
 		break;
 	}

commit e725def62c73b4aa269fefc4c0d96abb41927fcb

exportfs: Do not fail on empty exports file.
Commit 076dd80 introduced a regression that causes
exportfs to fail when there is an empty /etc/exports
file. A empty /etc/exports file is valid and should
not cause exportfs to fail.

Signed-off-by: Steve Dickson <steved@redhat.com>
Signed-off-by: Steve Dickson <steved@redhat.com>

support/export/export.c
@@ -71,12 +71,13 @@ static void warn_duplicated_exports(nfs_export *exp, struct exportent *eep)
  *
  */
 void
-export_read(char *fname)
+export_read(char *fname, int verbose)
 {
 	struct exportent	*eep;
 	nfs_export		*exp;
 
 	int volumes = 0;
+	int bad_entry = 0;
 
 	setexportent(fname, "r");
 	while ((eep = getexportent(0,1)) != NULL) {
@@ -85,13 +86,19 @@ export_read(char *fname)
 			exp = export_create(eep, 0);
 			if (exp)
 				volumes++;
+			else 
+				bad_entry++;
 		}
 		else
 			warn_duplicated_exports(exp, eep);
 	}
 	endexportent();
-	if (volumes == 0)
-		xlog(L_ERROR, "No file systems exported!");
+	if (volumes == 0) {
+		if (bad_entry > 0)
+			xlog(L_ERROR, "No file systems exported!");
+		else if (verbose)
+			xlog(L_WARNING, "No file systems exported!");
+	}
 }
 
 /**

support/include/exportfs.h
@@ -133,7 +133,7 @@ struct addrinfo *		client_resolve(const struct sockaddr *sap);
 int 				client_member(const char *client,
 						const char *name);
 
-void				export_read(char *fname);
+void				export_read(char *fname, int verbose);
 void				export_reset(nfs_export *);
 nfs_export *			export_lookup(char *hname, char *path, int caconical);
 nfs_export *			export_find(const struct addrinfo *ai,

utils/exportfs/exportfs.c
@@ -47,7 +47,7 @@ static void	error(nfs_export *exp, int err);
 static void	usage(const char *progname, int n);
 static void	validate_export(nfs_export *exp);
 static int	matchhostname(const char *hostname1, const char *hostname2);
-static void	export_d_read(const char *dname);
+static void	export_d_read(const char *dname, int verbose);
 static void grab_lockfile(void);
 static void release_lockfile(void);
 
@@ -182,8 +182,8 @@ main(int argc, char **argv)
 	atexit(release_lockfile);
 
 	if (f_export && ! f_ignore) {
-		export_read(_PATH_EXPORTS);
-		export_d_read(_PATH_EXPORTS_D);
+		export_read(_PATH_EXPORTS, f_verbose);
+		export_d_read(_PATH_EXPORTS_D, f_verbose);
 	}
 	if (f_export) {
 		if (f_all)
@@ -686,7 +686,7 @@ out:
 /* Based on mnt_table_parse_dir() in
    util-linux-ng/shlibs/mount/src/tab_parse.c */
 static void
-export_d_read(const char *dname)
+export_d_read(const char *dname, int verbose)
 {
 	int n = 0, i;
 	struct dirent **namelist = NULL;
@@ -729,7 +729,7 @@ export_d_read(const char *dname)
 			continue;
 		}
 
-		export_read(fname);
+		export_read(fname, verbose);
 	}
 
 	for (i = 0; i < n; i++)